d41b2e5abb0493ed52c2332fbeec0b7d8bde1d6f
ta-whmct <p.tatyana.andreevna@gmail.com>
2026-08-11 20:46:30 +0400

    rename router hello

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ routes = web.RouteTableDef()
 redis_service = RedisService
 
 
-@routes.get("/")
-async def hello(_: web.Request) -> web.Response:
+@routes.get("/fixed-window")
+async def fixes_window(_: web.Request) -> web.Response:
     client_ip = _.remote or "-"
     username = _.query.get("username", "-")
 
@@ -15,10 +15,10 @@ async def hello(_: web.Request) -> web.Response:
         await redis_service.check_limit_with_fixed_window_counter(f"{username}:{client_ip}")
         return web.Response(text="Hello, world")
     except ValueError:
-        return web.Response(text="limit", status=429)
+        return web.Response(text="request limit reached", status=429)
     except Exception:
         raise
 
 
 app = web.Application()
-app.add_routes([web.get("/", hello)])
+app.add_routes([web.get("/", fixes_window)])
